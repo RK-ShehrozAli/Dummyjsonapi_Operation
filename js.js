8daad80c40a65edb28a81f9adc106e9e78a4a333
Shehroz Ali <155454242+RK-ShehrozAli@users.noreply.github.com>
2024-03-07 13:43:36 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+document.getElementById("save_form").addEventListener("click", function (e) {
+  e.preventDefault();
+  var obj = {
+    id: document.getElementById("userid").value,
+    title: document.getElementById("title.text").value,
+    price: document.getElementById("price.text").value,
+    brand: document.getElementById("brand.text").value,
+  };
+  fetch("https://dummyjson.com/products/add",{
+    method: "PUT",
+    body: JSON.stringify(obj),
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+});
